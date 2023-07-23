d612d81e9f5f7c70f2bbee22f934f1ecc489bce5
Azamat32 <azamat.chimchkov@gmail.com>
2023-07-23 12:42:21 +0600

    adding the server

--- server/index.js
+++ server/index.js
@@ -1,10 +1,26 @@
-const express = require('express')
-const app = express()
-const port = 3000
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-  })
-  
-  app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-  })
+const express = require("express");
+const sequelize = require("./db");
+const models = require("./models/model");
+const cors = require("cors");
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+const start = async () => {
+  try {
+    await sequelize.authenticate();
+    await sequelize.sync();
+    app.listen(PORT, () =>
+      console.log(`Server is started succesfully on port ${PORT}`)
+    );
+  } catch (e) {
+    console.log(e);
+  }
+};
+start();
---
+++ server/models/model.js
@@ -0,0 +1,145 @@
+const { Sequelize, DataTypes } = require("sequelize");
+const sequelize = require("../db");
+
+const User = sequelize.define("User", {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  phone_number: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  username: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  role: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    defaultValue: "Обычный пользователь",
+  },
+});
+
+const Product = sequelize.define("Product", {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  type: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  price: {
+    type: DataTypes.DECIMAL,
+    allowNull: false,
+  },
+});
+
+const Basket = sequelize.define("Basket", {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+
+  created_at: {
+    type: DataTypes.DATE,
+    allowNull: false,
+    defaultValue: Sequelize.NOW,
+  },
+});
+
+const BasketItems = sequelize.define("BasketItems", {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  quantity: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    defaultValue: 1,
+  },
+});
+
+const Order = sequelize.define("Order", {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  created_at: {
+    type: DataTypes.DATE,
+    allowNull: false,
+    defaultValue: Sequelize.NOW,
+  },
+  total_amount: {
+    type: DataTypes.DECIMAL,
+    allowNull: false,
+  },
+  status: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+});
+
+const Payment = sequelize.define("Payment", {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  payment_date: {
+    type: DataTypes.DATE,
+    allowNull: false,
+    defaultValue: Sequelize.NOW,
+  },
+  amount: {
+    type: DataTypes.DECIMAL,
+    allowNull: false,
+  },
+  payment_method: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+});
+
+// User-Order association (One-to-Many)
+User.hasMany(Order, { foreignKey: "user_id" });
+Order.belongsTo(User, { foreignKey: "user_id" });
+
+// Order-Payment association (One-to-One)
+Order.hasOne(Payment, { foreignKey: "order_id" });
+Payment.belongsTo(Order, { foreignKey: "order_id" });
+
+// User-Basket association (One-to-Many)
+User.hasMany(Basket, { foreignKey: "user_id" });
+Basket.belongsTo(User, { foreignKey: "user_id" });
+
+// Basket-BasketItems association (One-to-Many)
+Basket.hasMany(BasketItems, { foreignKey: "basket_id" });
+BasketItems.belongsTo(Basket, { foreignKey: "basket_id" });
+
+// BasketItems-Product association (Many-to-One)
+BasketItems.belongsTo(Product, { foreignKey: "product_id" });
+Product.hasMany(BasketItems, { foreignKey: "product_id" });
+
+module.exports = {
+  User,
+  Basket,
+  BasketItems,
+  Order,
+  Payment,
+  Product,
+};
