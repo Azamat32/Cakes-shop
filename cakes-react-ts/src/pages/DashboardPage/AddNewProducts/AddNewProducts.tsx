c51577a5cbd3f42b8642613c65c742934b5cac3b
Azamat32 <azamat.chimchkov@gmail.com>
2023-07-24 15:14:54 +0600

    adding product form

--- cakes-react-ts/src/pages/DashboardPage/AddNewProducts/AddNewProducts.tsx
+++ cakes-react-ts/src/pages/DashboardPage/AddNewProducts/AddNewProducts.tsx
@@ -1,9 +1,25 @@
-
+import "./addProduct.scss"
 type Props = {}
 
 const AddNewProducts = (_props: Props) => {
   return (
-    <div>AddNewProducts</div>
+    <div className="addProduct">
+      <h1>Добавить новый товар</h1>
+      <form action="/" method="POST" className="addProductForm">
+        <div className="inputGroup">
+          <label >Имя товара</label>
+          <input type="text" placeholder="Имя товара" />
+        </div>
+        <div className="inputGroup">
+          <label >Цена</label>
+          <input type="Number" placeholder="Цена" />
+        </div>
+        <div className="inputGroup">
+          <label >Цена</label>
+          <input type="file" placeholder="Выберите картинку" />
+        </div>
+      </form>
+    </div>
   )
 }
 
